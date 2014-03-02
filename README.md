c7700aed956365333309c19e27a0b885d84e819d
Florian Letsch <florianletsch@gmail.com>
2014-03-02 15:13:53 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,17 @@
+graph.js
+===
+
+Just playing around with HTML5 canvas for the first time. You can use this for displaying
+graphs in your browser. Graphs are meant in the mathematical term of nodes and edged. Forget
+charts if you had that in mind.
+
+Usage
+===
+
+Define your graph as a plain JS object.
+
+```
+
+```
+
+Check out graph.js for an example.
---
+++ graph.js
@@ -0,0 +1,77 @@
+var GRAPH = function(){
+    var WIDTH, HEIGHT;
+    var PADDING = 10;
+    var NODE_RADIUS = 7;
+    var EDGE_WIDTH = 3;
+    var NODE_BORDER_WIDTH = 2;
+
+    function randBetween(lower, upper) {
+        return Math.floor(Math.random()*(upper-lower+1)+lower);
+    }
+
+    function drawNode(node, ctx, fillStyle, strokeStyle) {
+        ctx.beginPath();
+        ctx.moveTo(node.x+NODE_RADIUS, node.y);
+        ctx.strokeStyle = strokeStyle;
+        ctx.fillStyle = fillStyle;
+        ctx.lineWidth = NODE_BORDER_WIDTH;
+        ctx.arc(node.x, node.y, NODE_RADIUS, 0, Math.PI*2, true);
+        ctx.moveTo(node.x+NODE_RADIUS, node.y);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function drawEdges (node, graph, ctx, style) {
+        ctx.beginPath();
+        ctx.strokeStyle = style;
+        ctx.fillStyle = style;
+        ctx.lineWidth = EDGE_WIDTH;
+        for (var j in node.adj) {
+            var otherNode = graph[node.adj[j]];
+            ctx.moveTo(node.x, node.y);
+            ctx.lineTo(otherNode.x, otherNode.y);
+        }
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function draw (canvas, graph) {
+          var ctx = canvas.getContext("2d");
+          WIDTH = parseInt(canvas.getAttribute("width"), 10);
+          HEIGHT = parseInt(canvas.getAttribute("height"), 10);
+
+          // position nodes
+          for (var i in graph) {
+              var node = graph[i];
+              node.x = randBetween(PADDING, WIDTH-PADDING);
+              node.y = randBetween(PADDING, HEIGHT-PADDING);
+          }
+
+          // draw edges
+          for (var i in graph) {
+              var node = graph[i];
+              drawEdges(node, graph, ctx, "#aaa");
+          }
+          // draw nodes
+          for (var i in graph) {
+              var node = graph[i];
+              drawNode(node, ctx, "#ccc", "#666");
+          }
+    }
+
+    return {
+        draw: draw
+    }
+}();
+
+
+window.onload = function() {
+    var graphObj = {0: {adj: [1]}, 1: {adj: [2]}, 2: {adj: [0]}, 3: {adj: [0, 1]}};
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        GRAPH.draw(canvas, graphObj);
+    } else {
+        console.log('no canvas support');
+    }
+};
